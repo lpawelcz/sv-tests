c82ec398a5df95cb64b3ddf50829e5ea03b26c57
Pawel Czarnecki <pczarnecki@antmicro.com>
2021-02-04 15:13:59 +0100

    change naming format

--- .github/workflows/analyze.sh
+++ .github/workflows/analyze.sh
@@ -4,7 +4,7 @@ ANALYZER=$PWD"/tools/report_analyzer.py"
 OUT_DIR=$PWD"/out/report/"
 COMPARE_REPORT=$OUT_DIR"/report.csv"
 BASE_REPORT=$OUT_DIR"/base_report.csv"
-CHANGES_SUMMARY=$OUT_DIR"/changes_summary.json"
+CHANGES_SUMMARY=$OUT_DIR"/"$JOB_NAME"_changes_summary.json"
 
 set -x
 set -e
@@ -13,6 +13,8 @@ set -e
 wget https://symbiflow.github.io/sv-tests-results/report.csv -O $BASE_REPORT
 
 python $ANALYZER $COMPARE_REPORT $BASE_REPORT -o $CHANGES_SUMMARY
+
+mv $COMPARE_REPORT $OUT_DIR"/"$JOB_NAME"_report.csv"
 
 set +e
 set +x
